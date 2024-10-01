c904a4a16073338fa0772219e499e0d083a90dee
Alberto Lazzeri <albilazze2002@gmail.com>
2024-10-01 15:30:34 +0200

    up lec 7

---
+++ lezione_7/NSL_SIMULATOR/SOURCE/NSL_SIMULATOR.cpp
@@ -0,0 +1,92 @@
+/****************************************************************
+*****************************************************************
+    _/    _/  _/_/_/  _/       Numerical Simulation Laboratory
+   _/_/  _/ _/       _/       Physics Department
+  _/  _/_/    _/    _/       Universita' degli Studi di Milano
+ _/    _/       _/ _/       Prof. D.E. Galli
+_/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
+*****************************************************************
+*****************************************************************/
+
+#include <iostream>
+#include <cmath>
+#include "system.h"
+
+using namespace std;
+
+void Progress_Bar(int progress, int total, int bar_width=50) {
+
+    float percentage = static_cast<float>(progress) / total;
+    int pos = static_cast<int>(bar_width * percentage);
+
+    std::string bar;
+    for (int i = 0; i < bar_width; ++i) {
+        if (i < pos) bar += "=";
+        else if(i==pos) bar += ">";
+        else bar += " ";
+    }
+
+    std::cout<< "|" << bar << "| " << setprecision(3) << int(percentage * 100.0)<< "%\r";
+    std::fflush(stdout);
+}
+
+int main (int argc, char *argv[]){
+
+  if(argc!=2){
+		cerr << "ERR: system phase required" << endl;
+		exit(-1);
+	}
+  string phase=argv[1];
+
+  int nconf = 1;
+  System SYS;
+  SYS.initialize_equilibrated(phase);
+  SYS.initialize_properties();
+  SYS.block_reset(0);
+
+  //setting M(RT)^2 step lenght
+  if(SYS.get_sim_type()==1){
+    SYS.set_delta();
+  }
+
+  //equilibration
+  if(SYS.get_sim_type()==0){
+    for(int k=0; k<5000; k++){
+      SYS.step();
+      SYS.measure();
+    }
+  }
+  
+  SYS.block_reset(0);
+
+  for(int i=0; i < SYS.get_nbl(); i++){ //loop over blocks
+    for(int j=0; j < SYS.get_nsteps(); j++){ //loop over steps in a block
+      SYS.step();
+      SYS.measure();
+      if(j%10 == 0){
+//        SYS.write_XYZ(nconf); //Write actual configuration in XYZ format //Commented to avoid "filesystem full"! 
+        nconf++;
+      }
+    }
+    SYS.averages(i+1);
+    SYS.block_reset(i+1);
+
+    //adding progress bar
+    Progress_Bar(i,SYS.get_nbl());
+
+  }
+  SYS.finalize();
+  cout << endl;
+
+  return 0;
+}
+
+/****************************************************************
+*****************************************************************
+    _/    _/  _/_/_/  _/       Numerical Simulation Laboratory
+   _/_/  _/ _/       _/       Physics Department
+  _/  _/_/    _/    _/       Universita' degli Studi di Milano
+ _/    _/       _/ _/       Prof. D.E. Galli
+_/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
+*****************************************************************
+*****************************************************************/
