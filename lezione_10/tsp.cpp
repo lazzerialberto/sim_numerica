37edd2beae68cd58e6221032addce27dbb95bcfc
Alberto Lazzeri <albilazze2002@gmail.com>
2024-11-02 16:35:55 +0100

    update notebook 10

--- lezione_10/tsp.cpp
+++ lezione_10/tsp.cpp
@@ -45,8 +45,6 @@ int main(int argc,char*argv[]){
 
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-    // Ottieni il rank del processo corrente
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
